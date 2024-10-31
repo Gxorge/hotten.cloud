839fde7832f2450058a3f54f8c4e9cbfebdb7324
George Hotten <github@ghott.me>
2024-10-31 00:40:11 +0000

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import '../app.scss';
+</script>
+
+<main class="gweb-container">
+    <slot />
+</main>
+
+<footer class="gweb-footer footer-sticky">
+    <div class="content footer-text has-text-centered">
+        <div class="gweb-text-layout">
+            <p style="font-size: 15px;"><b>© George Hotten 2024</b></p>
+            <p style="font-size: 15px;" class="gweb-link-white">
+                <a href="https://github.com/gxorge/gweb3" target="_blank" rel="noopener">source</a> &middot;
+                <a href="https://github.com/gxorge/" target="_blank" rel="noopener">github</a> &middot;
+                <a href="https://hotten.uk" target="_blank" rel="noopener">hotten.uk</a>
+            </p>
+        </div>
+        <div class="footer-colours">
+            <div class="footer-colour" id="aston"></div>
+            <div class="footer-colour" id="solihull"></div>
+            <div class="footer-colour" id="george"></div>
+            <div class="footer-colour" id="hotten"></div>
+        </div>
+    </div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import HeroTitle from '$lib/HeroTitle.svelte';
+</script>    
+
+<svelte:head>
+    <title>The Hotten Cloud</title>
+</svelte:head>
+
+<HeroTitle title="The Hotten Cloud" subtitle="Infrastructure powered by hotten.uk"/>
+<section class="gweb-text-layout">
+    <div class="content">
+        <p>The Hotten Cloud is powered by 8 different servers and networking equipment. It provides a wide range of services to family and friends, from a media service to a security system.</p>
+
+        <h2>The History</h2>
+        <p>
+            The Hotten Cloud started in January 2022 with a single Raspberry Pi 3. It simply ran Pi-hole and an NGINX web server to host websites. This is still used today, however, now solely running AdGuard. A few months later, in March, I bought a LENOVO ThinkCentre M700 Tiny to act as a primary server for the network. It initially ran docker containers, whilst also storing ripped media and hosting a Jellyfin instance.
+            <br><br>
+            In 2023, I bought a rack. I installed a MikroTik 3011UIAS router and a CSS326-24G-2S switch. I also bought a Synology DS220+ network-attached-storage (NAS) server. The Lenovo M700 was moved into the rack and had all the media transferred to the NAS. In December, I bought Lenovo ThinkCentre M900. At the time, this server ran Hyper-V hosting a Windows Server instance along side a few Linux virtual machines for tinkering.
+            <br><br>
+            Into 2024, the new Lenovo M900 now runs as a dedicated web server running Debian and Coolify. In June, I introduced a UniFi Cloud Key Gen 2 into the rack, acting as a storage server for my two UniFi G5 flex cameras. In October, I switched my home's Wi-Fi to UniFi with a U6 Long Range. 
+        </p>
+
+        <h2>This site is still in progress</h2>
+        <p>It is planned to contain a detailed list of services, along side the equipment used.</p>
+    </div>
+</section>
